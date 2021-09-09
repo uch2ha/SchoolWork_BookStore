1e124f20fb2666495a2131dc01ce6886bb651186
uch2ha <sinyavskiy.d@gmail.com>
2021-09-09 12:34:53 +0300

    last test

--- src/main/java/swd20/bookStore/Web/bookStoreController.java
+++ src/main/java/swd20/bookStore/Web/bookStoreController.java
@@ -2,15 +2,16 @@ package swd20.bookStore.Web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@ResponseBody
+
 public class bookStoreController {
 
-	@RequestMapping("/hello")
+	@RequestMapping(value = "/bookStoreIndex", method = RequestMethod.GET)
 	public String hello() {
 
-		return "Hello World";
+		
+		return "bookStoreIndex";
 	}
 }
