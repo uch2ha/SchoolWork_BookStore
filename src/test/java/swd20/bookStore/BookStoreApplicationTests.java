d209bf489ea3ba41ebac287e3f72d4c07f68b905
uch2ha <sinyavskiy.d@gmail.com>
2021-09-09 12:21:07 +0300

    First test

---
+++ src/test/java/swd20/bookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package swd20.bookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
